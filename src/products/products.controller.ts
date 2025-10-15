23a148cd408bbfb5b2872a575d22a1dd5a185cbf
Abulai Rakhatuly <rakhatulyabylai@gmail.com>
2025-10-15 20:36:35 +0500

    updated version

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,50 @@
-import { Controller, Get } from '@nestjs/common';
-import { ProductsService } from './products.service';
+import { Role } from 'src/guards/roleDecorator';
+import { RoleGuard } from 'src/guards/roleGuard';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ProductDto } from './productDto';
+import { ProductService } from './products.service';
+import { JwtAuthGuard } from 'src/guards/jwtAuthGuard';
 
-@Controller('products')
-export class ProductsController {
-  constructor(private productServ: ProductsService) {}
+@Controller('product')
+export class ProductController {
+  constructor(private readonly prodServ: ProductService) {}
+
+  @Get('products')
+  async getAllProducts(@Query('page') page = 1, @Query('limit') limit = 3) {
+    return await this.prodServ.pagination(page, limit);
+  }
 
   @Get('getAll')
-  async getAll() {
-    return await this.productServ.getAll();
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Role('admin')
+  async getAll(
+    @Query('minPrice') minPrice?: number,
+    @Query('maxPrice') maxPrice?: number,
+    @Query('word') word?: string,
+  ) {
+    return await this.prodServ.getProd(minPrice, maxPrice, word);
+  }
+
+  @Post('create')
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Role('admin')
+  async create(@Body() dto: ProductDto) {
+    return await this.prodServ.createProd(dto);
+  }
+
+  @Delete('delProd/:id')
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Role('admin')
+  async del(@Param('id') id: number) {
+    return await this.prodServ.delete(Number(id));
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Product } from './entity/products-entity';
-import { Repository } from 'typeorm';
+import { Product } from './products.entity';
+import {
+  Between,
+  ILike,
+  LessThanOrEqual,
+  MoreThanOrEqual,
+  Repository,
+} from 'typeorm';
+import { ProductDto } from './productDto';
 
 @Injectable()
-export class ProductsService {
+export class ProductService {
   constructor(
-    @InjectRepository(Product) private productRepo: Repository<Product>,
+    @InjectRepository(Product) private prodRepo: Repository<Product>,
   ) {}
 
-  async getAll() {
-    return await this.productRepo.find();
+  async pagination(page: number, limit: number) {
+    const skip = (page - 1) * limit;
+
+    const [data, total] = await this.prodRepo.findAndCount({
+      skip,
+      take: limit,
+      order: { id: 'DESC' },
+    });
+    return {
+      data,
+      total,
+      page,
+      limit,
+      totalPage: Math.ceil(total / limit),
+    };
+  }
+
+  async getProd(minPrice?: number, maxPrice?: number, word?: string) {
+    const where: any = {};
+
+    if (minPrice && maxPrice) {
+      where.price = Between(minPrice, maxPrice);
+    } else if (minPrice) {
+      where.price = MoreThanOrEqual(minPrice);
+    } else if (maxPrice) {
+      where.price = LessThanOrEqual(maxPrice);
+    }
+
+    if (word) {
+      where.name = ILike(`%${word}%`);
+    }
+
+    return await this.prodRepo.find({ where, order: { price: 'DESC' } });
+  }
+
+  async createProd(dto: ProductDto) {
+    const product = await this.prodRepo.create(dto);
+    await this.prodRepo.save(product);
+    return product;
+  }
+
+  async delete(id: number) {
+    const product = await this.prodRepo.findOne({ where: { id } });
+    if (!product) throw new NotFoundException('Not found');
+
+    await this.prodRepo.remove(product);
+    return { message: `${product.name} deleted successfully` };
   }
 }
